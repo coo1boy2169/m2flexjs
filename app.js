5ae03888924bed7d536a11d9db14a83995162486
Coo1boy2169 <>
2023-12-01 14:10:05 +0100

    op2

---
+++ app.js
@@ -0,0 +1,19 @@
+const gamestate_start=0
+const gamestate_ingame=1
+const gamestate_gameover=2
+
+
+const ingamestate_start =0
+const ingamestate_roll=1
+const ingamestate_end =0
+
+let boardPositionSize =50
+let pawPosition= []
+let boardPosition=[]
+let plauerAmountButtons = []
+
+
+
+let canvas = document.getElementById("canvas")
+let g = canvas.getContext("2d")
+
