2c7485f071ce0321216c821ab1c2a3bb0398021e
Coo1boy2169 <>
2024-01-19 14:41:57 +0100

    Inleveren

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const ingamestate_start = 0
 const ingamestate_roll = 1
 const ingamestate_end = 0
 
+let lastRoll = -1
 let images = {}
 let gameState = gamestate_start;
 let ingameState = ingamestate_start;
@@ -53,14 +54,14 @@ function clearCanvas() {
 function draw() {
     clearCanvas();
     drawGameOver()
-    drawIngame()
 
 
     if (gameState === gamestate_start) {
         drawGameStart()
     }
     if (gameState === gamestate_ingame) {
-
+        drawIngame();
+        drawUI();
     }
 
 }
@@ -100,7 +101,7 @@ function initGame() {
     for (let ButtonNum = 0; ButtonNum < 4; ButtonNum++) {
         let button = new Rect(uiWindow.x + 5 + (50 * ButtonNum), uiWindow.y + 50, 50, 50)
         playerAmountButtons.push(button)
-        button.playerAmountButtons = ButtonNum + 1
+        button.playerAmount = ButtonNum + 1
     }
 
 
@@ -127,16 +128,46 @@ function drawGameStart() {
 
 }
 function drawIngame() {
+
+
+
+
+
+
     for (let i = 0; i < boardPositions.length; i++) {
+
 
         let pos = boardPositions[i]
 
+
         g.fillStyle = "green";
         g.fillRect(pos.x, pos.y, pos.w, pos.h);
-        g.fillStyle = "blue"
+        g.fillStyle = "white"
         g.fillText((i + 1) + "", pos.x, pos.y + 20)
 
     }
+
+    for (let i = 0; i < pawPosition.length; i++) {
+
+
+        let pos = pawPosition[i]
+        let boardI = pos.boardI
+
+        let boardpos = boardPositions[boardI] 
+        let PawnSize = boardPositionSize / 2;
+
+
+
+            // vraag waroom dit niet smaen werkt !>!>!>!>!>!>
+        g.drawImage(images["snakes.png"], 0,70, 600, 600)
+        g.drawImage(images["pawn"+i+".png"])
+        g.fillStyle = "green";
+        g.fillRect(boardpos.x, boardpos.y, boardpos.w, boardpos.h);
+        g.fillStyle = "blue"
+        g.fillText((i + 1) + "", boardpos.x, boardpos.y + 20)
+       
+    }
+ 
 
 }
 function drawGameOver() {
@@ -174,26 +205,69 @@ function loadImages() {
 }
 
 
+
+function startRoll() {
+    ingameState = ingamestate_roll
+    lastRoll = -1
+    draw()
+    setTimeout(endRoll, 500);
+}
+
+
+function endRoll() {
+    ingameState = ingamestate_end
+    let random = Math.floor(Math.random() * 6)
+    lastRoll = random
+    draw();
+
+}
+
+function drawUI() {
+    if (ingameState == ingamestate_roll) {
+        if (lastRoll == -1) {
+        }
+        //if (                                                                                                                                                                                                                                                                                                                ) {
+        //    g.fillText("rollen....", 20, 20);
+        //
+        //}
+    }
+    else {
+        "dice" + lastRoll + ".png"
+    }
+}
+
+
+
+
 function canvasClicked(mouseEvent) {
 
+
+
     for (let i = 0; i < playerAmountButtons.length; i++) {
-        let button = playerAmountButtons[i]
+
+        let button = playerAmountButtons[i];
+
+        let mX = mouseEvent.clientX
+        let mY = mouseEvent.clientY
+
+        let hitButton = inRect(mX, mY, button);
 
 
-        if (gameState == gamestate_start) {
+        if (hitButton === true) {
 
-        }
-        if (hitButton === true) { 
-            startGame(button.playerAmount);
+            if (gameState == gamestate_start) {
+                startGame(button.playerAmount);
+
+            }
+            else if (gameState == gamestate_ingame) {
+                startRoll();
+            }
             break;
         }
 
     }
 
-    let mX = mouseEvent.clienX
-    let mY = mouseEvent.clienY
 
-    let hitButton = inRect(mX, mY, button);
 
 }
 
@@ -211,8 +285,6 @@ function imageLoaded() {
 
 function inRect(px, py, rect) {
     let result = (px >= rect.x && px <= rect.x2 && py >= rect.y && py <= rect.y2)
-
-
     return result;
 
 }
@@ -222,17 +294,22 @@ function startGame(playerAmount) {
 
 
     gameState = gamestate_ingame
-    ingameState =  ingamestate_start
-    pawPosition = []; 
+    ingameState = ingamestate_start
+    pawPosition = [];
     playerTurn = 0
     winner = -1
     console.log("palyerAmount" + playerAmount);
-    for ( let i = 0 ; i < playerAmount; i++){
-        
-    }
+    for (let i = 0; i < playerAmount; i++) {
 
-function createPawns(playerI) {
-    return { boardI: 0, playerI: playerI }
+        let Pawn = createPawn(i)
+        pawPosition.push(Pawn)
+
+    }
+    draw();
+
 }
+function createPawn(playerI) {
+    return { boardI: 0, playerI: playerI };
 }
+
 loadImages()
